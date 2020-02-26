5538a34fad294451af7abb8c0d97cb7f5c0e71b7
Joe Dempsey <jdemps22@students.kennesaw.edu>
2020-02-26 02:00:24 -0500

    Prepares for data migration

---
+++ StocksTestAPI/Data/StockContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StocksTestLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StocksTestAPI.Data
+{
+    public class StockContext : DbContext
+    {
+        public StockContext()
+        {
+
+        }
+
+        public DbSet<StockModel> Stocks { get; set; }
+    }
+}
--- StocksTestLibrary/Data/AlphaVantageStock.cs
+++ StocksTestLibrary/Data/AlphaVantageStock.cs
@@ -25,48 +25,52 @@ namespace StocksTestLibrary.Data
             return json;
         }
 
-        public static IList<StockModel> FormatInfo(string ticker, string apikey)
+        public static IList<StockModel> FormatInfo(string[] tickers, string apikey)
         {
-            string json = FetchStock(ticker, apikey);
-
-            JObject o = JObject.Parse(json);
-
-            IList<JToken> js = o["Time Series (Daily)"].ToList();
-
             IList<StockModel> list = new List<StockModel>();
-            foreach (var i in js)
+
+            foreach (string ticker in tickers)
             {
-                foreach (var j in i)
+                string json = FetchStock(ticker, apikey);
+
+                JObject o = JObject.Parse(json);
+
+                IList<JToken> js = o["Time Series (Daily)"].ToList();
+
+                foreach (var i in js)
                 {
-                    StockModel stock = new StockModel
+                    foreach (var j in i)
                     {
-                        Open = j["1. open"].ToObject<decimal>(),
-                        High = j["2. high"].ToObject<decimal>(),
-                        Low = j["3. low"].ToObject<decimal>(),
-                        Close = j["4. close"].ToObject<decimal>(),
-                        Volume = j["6. volume"].ToObject<long>(),
-                        Date = i.ToString().Substring(1, 10)
-                    };
-                    stock.Year = Int32.Parse(stock.Date.Substring(0, 4));
-                    stock.Month = Int32.Parse(stock.Date.Substring(5, 2));
-                    stock.Day = Int32.Parse(stock.Date.Substring(8, 2));
-                    stock.AveragePrice = (stock.Open + stock.High + stock.Low + stock.Close) / 4;
-                    stock.Ticker = ticker;
+                        StockModel stock = new StockModel
+                        {
+                            Open = j["1. open"].ToObject<decimal>(),
+                            High = j["2. high"].ToObject<decimal>(),
+                            Low = j["3. low"].ToObject<decimal>(),
+                            Close = j["4. close"].ToObject<decimal>(),
+                            Volume = j["6. volume"].ToObject<long>(),
+                            Date = i.ToString().Substring(1, 10)
+                        };
+                        stock.Year = Int32.Parse(stock.Date.Substring(0, 4));
+                        stock.Month = Int32.Parse(stock.Date.Substring(5, 2));
+                        stock.Day = Int32.Parse(stock.Date.Substring(8, 2));
+                        stock.AveragePrice = (stock.Open + stock.High + stock.Low + stock.Close) / 4;
+                        stock.Ticker = ticker;
 
-                    list.Add(stock);
+                        list.Add(stock);
+                    }
                 }
-            }
 
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (i == 0)
-                    list[i].LastAveragePrice = list[i].AveragePrice;
-                else
-                    list[i].LastAveragePrice = list[i - 1].AveragePrice;
-                if (i == list.Count - 1)
-                    list[i].NextAveragePrice = list[i].AveragePrice;
-                else
-                    list[i].NextAveragePrice = list[i + 1].AveragePrice;
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (i == 0)
+                        list[i].LastAveragePrice = list[i].AveragePrice;
+                    else
+                        list[i].LastAveragePrice = list[i - 1].AveragePrice;
+                    if (i == list.Count - 1)
+                        list[i].NextAveragePrice = list[i].AveragePrice;
+                    else
+                        list[i].NextAveragePrice = list[i + 1].AveragePrice;
+                }
             }
 
             return list;
